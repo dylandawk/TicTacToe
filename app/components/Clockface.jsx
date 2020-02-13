dee66dc42fb466ff94d3c7c215fa45ade3f33859
Glitch (starter-react) <none>
2020-02-13 01:00:24 +0000

    ⛄️💄 Checkpoint ./app/components/Clockface.jsx:11355921/190 ./app/components/HelloWorld.jsx:11355921/113

---
+++ app/components/Clockface.jsx
@@ -0,0 +1,8 @@
+const React = require("react");
+
+function Clockface(props)
+{
+  return <h2>The time is {props.hours}</h2>
+}
+
+module.exports = Clockface;
--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -1,9 +1,13 @@
 const React = require("react");
 const util = require("../logic/util.js");
+const Clockface = require("./Clockface.jsx");
 
 function PageRoot() 
 {
-  return <h1>This is my page now! It belongs to {util.getName()}</h1>
+  return <div>
+    <h1>This is my page now! It belongs to {util.getName()}</h1>
+    <Clockface />
+  </div>
 }
 
 module.exports = PageRoot;
