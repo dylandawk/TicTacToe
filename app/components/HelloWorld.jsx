0e4e457214862a30df18e57acb7c35db0841bf42
Glitch (starter-react) <none>
2020-02-13 01:24:29 +0000

    ☎️💇 Checkpoint ./app/components/HelloWorld.jsx:11355921/232

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -4,19 +4,21 @@ const Clockface = require("./Clockface.jsx");
 
 function PageRoot() 
 {
-  const date = new Date();
+  const [date, setDate] = React.useState(new Date());
   
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(0); 
+  //useState returns a variable and a setter. it takes a starting value as an argument
   
   const doIncrement = () =>
   {
-      setCount = count +1;
+      setCount(count+1);
   };
   
   return <div>
     <h1>This is my page now! It belongs to {util.getName()}</h1>
     <h2>The current count is {count}</h2>
     <button onClick={doIncrement}>Increment</button>
+    <button onClick=[fetchTime}></button>]
     <Clockface hours = {date.getHours()} minutes = {date.getMinutes()} seconds = {date.getSeconds()} />
   </div>
 }
