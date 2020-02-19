c2d5b33872e8924408b869d75601f21de275b94b
Glitch (starter-react) <none>
2020-02-19 19:02:07 +0000

    ⏰🚄 Checkpoint ./app/components/HelloWorld.jsx:11355921/11517

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -1,34 +1,66 @@
 const React = require("react");
 const util = require("../logic/util.js");
-const Clockface = require("./Clockface.jsx");
+const PlayerStatus = require("./PlayerStatus.jsx");
 
-function PageRoot() 
-{
-  const [date, setDate] = React.useState(new Date());
-  const [count, setCount] = React.useState(0); 
-  //useState returns a variable and a setter. it takes a starting value as an argument
-  
-  const doIncrement = () =>
-  {
-      setCount(count+1);
-  };
-  
-  const setTime = () => {
-    setDate(new Date());
-    
+function PageRoot() {
+  const rows = 3;
+  const columns = 3;
+  const [grid, setGrid] = React.useState( [
+        [null, null, null],
+        [null,null, null],
+        [null,null, null],
+    ]);
+  const PLAYER_ONE = 0;
+  const PLAYER_TWO = 1;
+
+  const rowCrossed = () => {
+    for (let i=0; i<rows; i++) 
+    { 
+        if (grid[i][0] == grid[i][1] && 
+            grid[i][1] == grid[i][2] &&  
+            grid[i][0] != null) 
+            return (true); 
+    } 
+    return(false); 
   }
-  
-  React.useEffect(() => {
-    setInterval(setTime, 1000)
-  }, []);
-  
+
+  const columnCrossed = () => {
+    for (let i=0; i<columns; i++) 
+    { 
+        if (grid[0][i] == grid[1][i] && 
+            grid[1][i] == grid[2][i] &&  
+            grid[0][i] != null) 
+            return (true); 
+    } 
+    return(false); 
+  }
+
+  const diagonalCrossed = () => {
+
+    if (grid[0][0] == grid[1][1] && 
+      grid[1][1] == grid[2][2] &&  
+      grid[0][0] != null) 
+      return(true); 
+        
+    if (grid[0][2] == grid[1][1] && 
+        grid[1][1] == grid[2][0] && 
+        grid[0][2] != null) 
+        return(true); 
+
+    return(false); 
+  }
+
+  const gameOver = () => {
+    return(rowCrossed(grid) || columnCrossed(grid) || diagonalCrossed(grid) );
+  }
+
   return <div>
-    <h1>This is my page now! It belongs to {util.getName()}</h1>
-    <h2>The current count is {count}</h2>
-    <button onClick={doIncrement}>Increment</button>
-    <button onClick={setTime}>Fetch Time</button>
-    <Clockface hours = {date.getHours()} minutes = {date.getMinutes()} seconds = {date.getSeconds()} />
+    <h2>Hello World!</h2>
+    <br></br>
+    <h2>The Current Player is: </h2>
+    <button>{grid[0</button>
   </div>
+
 }
 
 module.exports = PageRoot;
