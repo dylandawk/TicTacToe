d20bd2c19a9b1cc57cf5d50b7834b8ba2c53d3a6
Glitch (starter-react) <none>
2020-02-19 21:44:30 +0000

    🚶🎐 Checkpoint ./app/components/HelloWorld.jsx:11355921/1137 ./app/components/PlayerStatus.jsx:11355921/445

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -5,15 +5,17 @@ const PlayerStatus = require("./PlayerStatus.jsx");
 function PageRoot() {
   const rows = 3;
   const columns = 3;
-  const [grid, setGrid] = React.useState( [
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-    ]);
+
   const [player, setPlayer] = React.useState("X");
   const [gameState, setGameState] = React.useState(0);
   const PLAYER_ONE = "X";
   const PLAYER_TWO = "O";
+  const EMPTY = "-";
+  const [grid, setGrid] = React.useState( [
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+  ]);
   
   const buttonStyle = {
     backgroundColor: "#4CAF50", /* Green */
@@ -31,7 +33,7 @@ function PageRoot() {
     { 
         if (grid[i][0] == grid[i][1] && 
             grid[i][1] == grid[i][2] &&  
-            grid[i][0] != "-") 
+            grid[i][0] != EMPTY) 
             return (true); 
     } 
     return(false); 
@@ -42,7 +44,7 @@ function PageRoot() {
     { 
         if (grid[0][i] == grid[1][i] && 
             grid[1][i] == grid[2][i] &&  
-            grid[0][i] != "-") 
+            grid[0][i] != EMPTY) 
             return (true); 
     } 
     return(false); 
@@ -52,12 +54,12 @@ function PageRoot() {
 
     if (grid[0][0] == grid[1][1] && 
       grid[1][1] == grid[2][2] &&  
-      grid[0][0] != "-") 
+      grid[0][0] != EMPTY) 
       return(true); 
         
     if (grid[0][2] == grid[1][1] && 
         grid[1][1] == grid[2][0] && 
-        grid[0][2] != "-") 
+        grid[0][2] != EMPTY) 
         return(true); 
 
     return(false); 
@@ -67,6 +69,15 @@ function PageRoot() {
   const gameOver = () => {
     return(rowCrossed(grid) || columnCrossed(grid) || diagonalCrossed(grid) );
   }
+  const resetGame = () => {
+    let gridCopy = [...grid];
+    for(let i = 0; i<rows; i++){
+      for(let j = 0; j<columns; j++){
+        gridCopy[i][j] = player;
+      }
+    }
+  }
+  
   
   //change button to one clicked
   const setTile = (row, column) => {
@@ -74,6 +85,7 @@ function PageRoot() {
     gridCopy[row][column] = player;
     setGrid(gridCopy);
   }  
+  
   
   //change player who goes next
   const setTurn = () => {
@@ -85,27 +97,30 @@ function PageRoot() {
   {
     let row = Math.floor(buttonClicked/rows);
     let column = buttonClicked % columns;
-    if(grid[row][column]== "-") setTile(row, column);
+    if(grid[row][column]== EMPTY) setTile(row, column);
     if(!gameOver()){
       setTurn();
+    }
+    else{
+      setGameState(1);
     }
   }
 
   return <div>
     <h2>Hello World!</h2>
     <br></br>
-    <PlayerStatus playerTurn = {player}/>
+    <PlayerStatus playerTurn = {player} gameState = {gameState}/>
     <button style = {buttonStyle} onClick = {() => playGame(0)}> {grid[0][0]} </button>
-    <button style = {buttonStyle} onClick = "playGame(1)"> {grid[0][1]} </button> 
-    <button style = {buttonStyle} onClick = "playGame(2)"> {grid[0][2]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(1)}> {grid[0][1]} </button> 
+    <button style = {buttonStyle} onClick = {() => playGame(2)}> {grid[0][2]} </button>
     <br></br>
-    <button style = {buttonStyle} onClick = "playGame(3)"> {grid[1][0]} </button>
-    <button style = {buttonStyle} onClick = "playGame(4)"> {grid[1][2]} </button>
-    <button style = {buttonStyle} onClick = "playGame(5)"> {grid[2][0]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(3)}> {grid[1][0]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(4)}> {grid[1][1]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(5)}> {grid[1][2]} </button>
     <br></br>
-    <button style = {buttonStyle} onClick = "playGame(6)"> {grid[2][1]} </button>
-    <button style = {buttonStyle} onClick = "playGame(7)"> {grid[2][2]} </button>
-    <button style = {buttonStyle} onClick = "playGame(8)"> {grid[2][2]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(6)}> {grid[2][0]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(7)}> {grid[2][1]} </button>
+    <button style = {buttonStyle} onClick = {() => playGame(8)}> {grid[2][2]} </button>
   </div>
 
 }
--- app/components/PlayerStatus.jsx
+++ app/components/PlayerStatus.jsx
@@ -2,7 +2,12 @@ const React = require("react");
 
 function PlayerStatus(props)
 {
-  return <h2>It is Player {props.playerTurn}'s Turn</h2>
+  if(props.gameState == 0){
+    return <h2>It is Player {props.playerTurn}'s Turn</h2>;
+  } else (if gameState =)
+  {
+      return <h2> Player {props.playerTurn} Won!</h2>
+  }
 }
 
 module.exports = PlayerStatus;
